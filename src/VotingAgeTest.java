073415ddd7ff6f51bf70b002b351512f9fdc7bf4
MukundPradhan <replyme.mmp@gmail.com>
2016-01-31 07:17:52 +0530

    Adding Class

---
+++ src/VotingAgeTest.java
@@ -0,0 +1,17 @@
+
+
+public class VotingAgeTest {
+	public static void main(String[] args){
+		
+		int inputAge = 18;
+		
+		
+		 if(inputAge>=18){
+			 System.out.println("Your are eligible to vote .");
+			  }
+		 else{
+			 System.out.println("You are not eligible to vote.");
+		 }
+	}
+
+}
